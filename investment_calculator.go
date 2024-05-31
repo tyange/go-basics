f04fd646ee7b531624d2f9c69a23bd4fb0999776
tyange <usun16@gmail.com>
2024-06-01 08:29:11 +0900

    add inflation rate

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 6.5
 	var investmentAmount float64 = 1000
 	var years float64 = 10
 	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	fmt.Print(futureValue)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
