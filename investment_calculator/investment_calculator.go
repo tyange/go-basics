3f4c5069ba20702b5b4763b528d649b68ee88740
tyange <usun16@gmail.com>
2024-06-01 20:14:44 +0900

    an alternative return value syntax

--- investment_calculator/investment_calculator.go
+++ investment_calculator/investment_calculator.go
@@ -34,9 +34,9 @@ func outputText(text string) {
 	fmt.Print(text)
 }
 
-func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	rfv := fv / math.Pow(1+inflationRate/100, years)
+func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
 
-	return fv, rfv
+	return
 }
