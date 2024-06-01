f05ebc3cd14cbe8e06d8c4572a26016ac3b556e8
tyange <usun16@gmail.com>
2024-06-01 22:34:27 +0900

    add error handler & write file logic to profit calculator

--- practice_1/profit_calculator.go
+++ practice_1/profit_calculator.go
@@ -1,17 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		showErrorMessage(err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil {
+		showErrorMessage(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+
+	if err != nil {
+		showErrorMessage(err)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
+	storeResult(ebt, profit, ratio)
+}
+
+func showErrorMessage(err error) {
+	fmt.Println("ERROR")
+	fmt.Println(err)
+	fmt.Println("-----------")
+}
+
+func storeResult(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -22,11 +55,15 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("invalid amount")
+	}
+
+	return userInput, nil
 }
