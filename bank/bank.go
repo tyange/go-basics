66164060e8737e2f2278149a1f9cff58515acb58
tyange <usun16@gmail.com>
2024-06-01 20:53:03 +0900

    first if statement

--- bank/bank.go
+++ bank/bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance = 1000.0
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
@@ -13,6 +15,10 @@ func main() {
 	var choice int
 	fmt.Print("Your choice: ")
 	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	}
 
 	fmt.Println("Your choice:", choice)
 }
