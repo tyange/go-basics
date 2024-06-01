db241aeee6b71cd57d20d14cbad7f7048c00e461
tyange <usun16@gmail.com>
2024-06-01 21:12:39 +0900

    add nested if statement

--- bank/bank.go
+++ bank/bank.go
@@ -22,15 +22,32 @@ func main() {
 		fmt.Print("Your deposit: ")
 		var depositAmount float64
 		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+
 		accountBalance += depositAmount
 		fmt.Println("Balance updated! New amount:", accountBalance)
 	} else if choice == 3 {
 		fmt.Print("Withdrawal amount: ")
 		var withdrawalAmount float64
 		fmt.Scan(&withdrawalAmount)
+
+		if withdrawalAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+
+		if withdrawalAmount > accountBalance {
+			fmt.Println("Invalid amount. You can't withdraw more than you have.")
+			return
+		}
+
 		accountBalance -= withdrawalAmount
 		fmt.Println("Balance updated! New amount:", accountBalance)
+	} else {
+		fmt.Println("Goodbye!")
 	}
-
-	fmt.Println("Your choice:", choice)
 }
