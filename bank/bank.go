47c5f36d2b5eb65db3469e5ee800082304f1fac4
tyange <usun16@gmail.com>
2024-06-01 21:55:14 +0900

    return error

--- bank/bank.go
+++ bank/bank.go
@@ -13,14 +13,14 @@ func getBalanceFromFile() (float64, error) {
 	data, err := os.ReadFile(accountBalanceFile)
 
 	if err != nil {
-		return 1000, errors.New("Failed to find balance file.")
+		return 1000, errors.New("failed to find balance file")
 	}
 
 	balanceText := string(data)
 	balance, err := strconv.ParseFloat(balanceText, 64)
 
 	if err != nil {
-		return 1000, errors.New("Failed to parse stored balance value.")
+		return 1000, errors.New("failed to parse stored balance value")
 	}
 
 	return balance, nil
