4c109d571cc66a257a0c63c5b649fcead2cdad8b
tyange <usun16@gmail.com>
2024-06-02 16:11:18 +0900

    using constructor function

--- structs/structs.go
+++ structs/structs.go
@@ -25,18 +25,21 @@ func (u *user) clearUserName() {
 	u.lastName = ""
 }
 
+func newUser(firstName, lastName, birthdate string) *user {
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	appUser := user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthDate: userBirthdate,
-		createdAt: time.Now(),
-	}
-
+	appUser := newUser(userFirstName, userLastName, userBirthdate)
 	// ... do something awesome with that gathered data!
 
 	appUser.outputUserDetails()
