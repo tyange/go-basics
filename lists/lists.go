335858ff340078c3296cd6a1b501564b7b73ad8b
tyange <usun16@gmail.com>
2024-06-05 08:21:15 +0900

    selcting slices

--- lists/lists.go
+++ lists/lists.go
@@ -19,6 +19,7 @@ func main() {
 
 	fmt.Println(prices[2])
 
-	featuredPrices := prices[1:3]
-	fmt.Println(featuredPrices)
+	featuredPrices := prices[1:]
+	highlightedPrices := featuredPrices[:1]
+	fmt.Println(highlightedPrices)
 }
