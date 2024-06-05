2ed41cca84ce650b8d02f8e7ae25acd2d706ffd6
tyange <usun16@gmail.com>
2024-06-06 00:01:05 +0900

    make function with maps

--- make_function/make_function.go
+++ make_function/make_function.go
@@ -10,4 +10,12 @@ func main() {
 	userNames = append(userNames, "Manuel")
 
 	fmt.Println(userNames)
+
+	counseRatings := make(map[string]float64, 3)
+
+	counseRatings["go"] = 4.7
+	counseRatings["react"] = 4.8
+	counseRatings["angular"] = 4.5
+
+	fmt.Println(counseRatings)
 }
