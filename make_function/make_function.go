e5ad13dad23f82e94a68478c101944d63353635c
tyange <usun16@gmail.com>
2024-06-06 00:04:00 +0900

    with type aliases

--- make_function/make_function.go
+++ make_function/make_function.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type floatMap map[string]float64
+
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
 	// slice type, slots, allocated spaces
 	userNames := make([]string, 2, 5)
@@ -11,11 +17,11 @@ func main() {
 
 	fmt.Println(userNames)
 
-	counseRatings := make(map[string]float64, 3)
+	counseRatings := make(floatMap, 3)
 
 	counseRatings["go"] = 4.7
 	counseRatings["react"] = 4.8
 	counseRatings["angular"] = 4.5
 
-	fmt.Println(counseRatings)
+	counseRatings.output()
 }
