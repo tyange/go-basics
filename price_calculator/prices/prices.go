f81894ac86ed50bdd2e6e5c1e56fcbf707c6e32a
tyange <usun16@gmail.com>
2024-06-06 21:17:48 +0900

    add goroutine to project

--- price_calculator/prices/prices.go
+++ price_calculator/prices/prices.go
@@ -34,11 +34,13 @@ func (job *TaxIncludedPriceJob) LoadData() error {
 	return nil
 }
 
-func (job *TaxIncludedPriceJob) Process() error {
+func (job *TaxIncludedPriceJob) Process(doneChan chan bool, errorChan chan error) {
 	err := job.LoadData()
 
 	if err != nil {
-		return err
+		// return err
+		errorChan <- err
+		return
 	}
 
 	result := make(map[string]string)
@@ -49,8 +51,8 @@ func (job *TaxIncludedPriceJob) Process() error {
 	}
 
 	job.TaxIncludedPrices = result
-
-	return job.IOManager.WriteResult(job)
+	job.IOManager.WriteResult(job)
+	doneChan <- true
 }
 
 func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
