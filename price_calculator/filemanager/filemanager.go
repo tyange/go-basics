ac191540697edab6a289cdb930be78649eb03f99
tyange <usun16@gmail.com>
2024-06-06 21:19:24 +0900

    using defer

--- price_calculator/filemanager/filemanager.go
+++ price_calculator/filemanager/filemanager.go
@@ -20,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("failed to open file")
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -47,16 +49,19 @@ func (fm FileManager) WriteResult(data interface{}) error {
 		return errors.New("failed to create file")
 	}
 
+	defer file.Close()
+
 	time.Sleep(3 * time.Second)
 
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
+		// file.Close()
 		return errors.New("failed to convert data to JSON")
 	}
 
-	file.Close()
+	// file.Close()
 	return nil
 }
 
