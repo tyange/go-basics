fb25f4f043906ea145d4db48058002716d8ba51d
tyange <usun16@gmail.com>
2024-06-07 23:37:16 +0900

    added auth middleware

--- rest_api/routes/events.go
+++ rest_api/routes/events.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 
 	"example.com/rest_api/models"
-	"example.com/rest_api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -39,22 +38,8 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		fmt.Println(err)
@@ -62,6 +47,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 
 	err = event.Save()
