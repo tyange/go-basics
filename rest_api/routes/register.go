e5f082fb95ed0a9dca5dad3d30ee42da6fa06793
tyange <usun16@gmail.com>
2024-06-08 00:35:56 +0900

    add registration

---
+++ rest_api/routes/register.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"example.com/rest_api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	err = event.Register(userId)
+
+	if err != nil {
+		fmt.Println(err)
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user for event."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Registered!"})
+}
+
+func cancelRegistration(context *gin.Context) {}
